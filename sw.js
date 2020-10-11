dc67d1652b612e7a9f9b7a15b4b14448690bd2b2
FirozeJi <firozecool@gmail.com>
2020-10-11 20:53:37 +0530

    whateva

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-// just a placeholder comment to ensure cache gets refresh. qanonmesdfasag..s
+// just a placeholder comment to ensure cache gets refresh. qanonmespo
 
 self.addEventListener("install", e => {
 e.waitUntil(
