81eb562aa1468eb8504307137e4adacd8a05f2b5
FirozeJi <firozecool@gmail.com>
2020-10-12 03:17:52 +0530

    a

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-// just a placeholder comment to ensure cache gets refresh. vvveaqfcx,q
+// just a placeholder comment to ensure cache gets refresh. vvveaqfcx,qq1
 
 self.addEventListener("install", e => {
 e.waitUntil(
